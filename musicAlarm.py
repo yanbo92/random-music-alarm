eb9f2fe6a3df831dd5570fa8a48bd99923d4cf60
hodorli <13750429758@139.com>
2021-10-10 13:52:37 +0800

    finish windows part

--- musicAlarm.py
+++ musicAlarm.py
@@ -1,8 +1,10 @@
 import os
 import platform
-import requests
-import pynput
+import keyboard
 from playsound import playsound
+from random import choice
+import schedule
+import time
 
 
 def mute():
@@ -17,14 +19,38 @@ def unmute():
         os.system("osascript -e 'set volume without output muted'")
     if platform.system() == 'Windows':
         os.system("nircmd.exe mutesysvolume 0")
+        os.system("nircmd.exe setsysvolume 50000")
 
 
 def play_music():
-    #  requests.get('https://api.vvhan.com/api/rand.music?sort=热歌榜')
-    r = requests.get('https://api.uomg.com/api/rand.music?sort=热歌榜&format=music')
-    with open("music.mp3", "wb") as code:
-        code.write(r.content)
-    playsound("music.mp3")
+    def try_play():
+        music = choice(music_list)
+        print("play {}".format(music))
+        try:
+            playsound("D:\Music" + "\\" + music)
+        except BaseException as e:
+            print("error" + str(e))
+            try_play()
 
 
-play_music()
+    unmute()
+    dir = "D:\Music"
+    music_list = []
+    for root, dirs, files in os.walk(dir, topdown=False):
+        for name in files:
+            music_list.append(name)
+            print(name)
+    try_play()
+    mute()
+
+
+schedule.every().day.at("7:50").do(play_music)
+
+schedule.every().day.at("09:00").do(play_music)
+schedule.every().day.at("09:10").do(play_music)
+schedule.every().day.at("09:20").do(play_music)
+
+keyboard.add_hotkey('k', mute)
+while True:
+    schedule.run_pending()
+    time.sleep(10)
